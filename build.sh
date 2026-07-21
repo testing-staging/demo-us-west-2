e45e6a9ff42d93af0c8cdb279f503847cca97fe3
Webslice Developers <developers@webslice.com>
2026-07-21 02:04:59 +0000

    e2e: introduce [cdn.redirects], [deploy.commands.build] and [deploy.commands.release] in settings.toml (#2829, #2920)

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "ae0537ecc120686b30944448db332eda"
---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "6d300950cb3e0cdb5957b915a4f4f42c"
+mkdir -p "$(dirname "public/release.html")"
+printf %s "51c1a7432a36ffd58ac4def2253cd7fb" > "public/release.html"
